ce9746d3e8178e404415862951d1132f6b23c154
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-19 06:10:49 +0530

    RoboShop TF Code

--- main.tf
+++ main.tf
@@ -34,18 +34,22 @@ resource "aws_lb" "main" {
   tags               = merge(var.tags, { Name = local.name })
 }
 
-#resource "aws_lb_listener" "main" {
-#  count             = var.enable_https ? 0 : 1
-#  load_balancer_arn = aws_lb.main.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = var.target_group_arn
-#  }
-#}
-#
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "500"
+    }
+  }
+}
+
 #resource "aws_lb_listener" "https" {
 #  count             = var.enable_https ? 1 : 0
 #  load_balancer_arn = aws_lb.main.arn
